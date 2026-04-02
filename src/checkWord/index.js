b1b3ae9597903f8764518a41c5c08062117e93ec
Edmorton1 <ghettoboy2106@gmail.com>
2026-04-02 22:47:03 +0500

    code cleaning

--- src/checkWord/index.js
+++ src/checkWord/index.js
@@ -1,9 +1,8 @@
 const isEmpty = require('./isEmpty');
 const isCancel = require('./isCancel');
-const isRepeat = require('./isRepeat');
 
 module.exports = (word, ext) => {
-  if (isEmpty(word, ext) || isCancel(word, ext) || isRepeat(word, ext)) {
+  if (isEmpty(word, ext) || isCancel(word, ext)) {
     return true;
   }
   return false;
--- src/translateWord/models/lingva.js
+++ src/translateWord/models/lingva.js
@@ -1,30 +1,5 @@
 const handler = require('./handler');
 
-// TODO: Удалить
-// const fetch = require('node-fetch').default;
-
-// /** @returns {Promise<string | null>} */
-// module.exports = async (original, {log}) => {
-//   if (res.status === 429) {
-//     log('Too many request. Try later, or change VPN network\n');
-
-//     return null;
-//   }
-
-//   if (res.status !== 200) {
-//     log('Lingva Translate server error. Try later\n');
-//     log(res);
-
-//     return null;
-//   }
-
-//   try {
-//     return (await res.json()).translation;
-//   } catch (err) {
-//     log(err);
-//   }
-// };
-
 module.exports = (original, ext) =>
   handler(
     `https://translate.plausibility.cloud/api/v1/en/ru/${original}`,
