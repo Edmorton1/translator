576cd09c108d72e8ec23f78ff1717f6e092a8656
Edmorton1 <ghettoboy2106@gmail.com>
2026-04-04 09:10:43 +0500

    add log

--- src/translateWord/index.js
+++ src/translateWord/index.js
@@ -2,7 +2,8 @@ const models = require('./models');
 
 /** @returns {Promise<string | null>} */
 module.exports = async (original, ext) => {
-  const {model} = ext;
+  const {model, log} = ext;
+  log('using model: ', model);
 
   return models[model](original, ext);
 };
